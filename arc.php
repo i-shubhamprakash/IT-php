a0f1c12f6996251f7b2d2dc31d729bbc29d14b38
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-09-27 06:02:07 +0530

    Format ARC page data

--- arc.php
+++ arc.php
@@ -1,5 +1,4 @@
-<?php
-/*
+<!--
 CREATED BY:
 ** BACK-END:
 Aditya Thakur, Piyush Vikas
@@ -10,69 +9,156 @@ Shubham Prakash
 LikedIn: https://www.linkedin.com/in/ishubhamprakash
 
 ALL RIGHTS RESERVED
-*/
-$page_requirements = '
-	<title>ARC | IT Dept</title>
-<link rel="stylesheet" href="styles/arc.css" type="text/css" />	';
+-->
+<?php
  	include("header.php");
 ?>
 
-<div class="main-body">
-	<div class="container-fluid  card">
-		<h1 class="heading">Anti-Ragging Cell</h1>
-		<h2 class="sub-heading"> Ragging constitutes one or more of any of the following acts: </h2>
+<div class="container main-body">
+	<div class="row">
+		<div class="col-12">
+			<h1 class="heading">Anti-Ragging Cell</h1>
+		</div>
+	</div>
 
-		<p>Any conduct by any student or students whether by words spoken or written or by an act which has the effect of
-			teasing, treating or handling with rudeness a fresher or any other student;<br>
-			Indulging in rowdy or undisciplined activities by any student or students which causes or is likely to cause
-			annoyance, hardship, physical or psychological harm or to raise fear or apprehension thereof in any fresher or any
-			other student;</p>
-			<p>
-			Asking any student to do any act which such student will not in the ordinary course do and which has the effect of
-			causing or generating a sense of shame, or torment or embarrassment so as to adversely affect the physique or psyche
-			of such fresher or any other student;<br>
-			Any act by a senior student that prevents, disrupts or disturbs the regular academic activity of any other student
-			or a fresher;
-			</p>
-			<p>			Exploiting the services of a fresher or any other student for completing the academic tasks assigned to an
-			individual or a group of students.
-			Any act of financial extortion or forceful expenditure burden put on a fresher or any other student by students;<br>
-			Any act of physical abuse including all variants of it: sexual abuse, homosexual assaults, stripping, forcing
-			obscene and lewd acts, gestures, causing bodily harm or any other danger to health or person;<br>
-			Any act or abuse by spoken words, emails, posts, public insults which would also include deriving perverted
-			pleasure, vicarious or sadistic thrill from actively or passively participating in the discomfiture to fresher or
-			any other student;
-			Any act that affects the mental health and self-confidence of a fresher or any other student with or without an
-			intent to derive a sadistic pleasure or showing off power, authority or superiority by a student over any fresher or
-			any other student.</p>
-<br>
-			<strong>Actions to be taken against students for indulging and abetting ragging in technical
-				institutions/Universities including Deemed to be University imparting technical education</strong><br>
+	<div class="row">
+		<div class="col-12">
+			<div class="boxyfy">
+				<h2 class="sub-heading"> Ragging constitutes one or more of any of the following acts: </h2>
+				<ul>
+					<li>
+						<p>
+							Any conduct by any student or students whether by words spoken or written or by an act which has the effect of
+							teasing, treating or handling with rudeness a fresher or any other student.
+						</p>
+					</li>
+					<li>
+						<p>
+							Indulging in rowdy or undisciplined activities by any student or students which causes or is likely to cause
+							annoyance, hardship, physical or psychological harm or to raise fear or apprehension thereof in any fresher or
+							any other student.
+						</p>
+					</li>
 
-				<p>	The punishment to be meted out to the persons indulged in ragging has to be exemplary and justifiably harsh to act
+					<li>
+						<p>
+							Asking any student to do any act which such student will not in the ordinary course do and which has the effect
+							of
+							causing or generating a sense of shame, or torment or embarrassment so as to adversely affect the physique or
+							psyche
+							of such fresher or any other student.
+						</p>
+					</li>
+
+					<li>
+						<p>
+							Any act by a senior student that prevents, disrupts or disturbs the regular academic activity of any other
+							student
+							or a fresher
+						</p>
+					</li>
+					<li>
+						<p>
+							Exploiting the services of a fresher or any other student for completing the academic tasks assigned to an
+							individual or a group of students.
+						</p>
+					</li>
+					<li>
+						<p> Any act of financial extortion or forceful expenditure burden put on a fresher or any other student by
+							students
+						</p>
+					</li>
+					<li>
+						<p>
+
+							Any act of physical abuse including all variants of it: sexual abuse, homosexual assaults, stripping, forcing
+							obscene and lewd acts, gestures, causing bodily harm or any other danger to health or person.
+						</p>
+					</li>
+					<li>
+						<p>Any act or abuse by spoken words, emails, posts, public insults which would also include deriving perverted
+							pleasure, vicarious or sadistic thrill from actively or passively participating in the discomfiture to fresher
+							or
+							any other student.</p>
+					</li>
+					<li>
+						<p>Any act that affects the mental health and self-confidence of a fresher or any other student with or without
+							an
+							intent to derive a sadistic pleasure or showing off power, authority or superiority by a student over any
+							fresher or
+							any other student.</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-12">
+			<div class="boxyfy">
+			<h2 class="sub-heading">Punishment for ragging:</h2>
+			<p class="h4">Actions to be taken against students for indulging and abetting ragging in technical
+			institutions/Universities including Deemed to be University imparting technical education</p>
+<ul>
+	<li>
+		<p>
+		The punishment to be meted out to the persons indulged in ragging has to be exemplary and justifiably harsh to
+			act
 			as a deterrent against recurrence of such incidents.<br>
 			Every single incident of ragging a First Information Report (FIR) must be filed without exception by the
-			institutional authorities with the local police authorities.</p>
-	<p></p>
-			The Anti-Ragging Committee of the institution shall take an appropriate decision, with regard to punishment or
-			otherwise, depending on the facts of each incident of ragging and nature and gravity of the incident of ragging.
-			Depending upon the nature and gravity of the offence as established the possible punishments for those found guilty
-			of ragging at the institution level shall be any one or any combination of the following,<br><br>
-			Cancellation of admission<br>
-			Suspension from attending classes<br>
-			Withholding / withdrawing scholarship / fellowship and other benefits
-			Debarring from appearing in any test / examination or other evaluation process
-			Withholding results<br>
-			Debarring from representing the institution in any regional, national or international meet, tournament, youth
-			festival, etc.<br>
-			Suspension / expulsion from the hostel<br>
-			Rustication from the institution for period ranging from 1 to 4 semesters
-			Expulsion from the institution and consequent debarring from admission to any other institution.<br>
-			Collective punishment: when the persons committing or abetting the crime of ragging are not identified, the
-			institution shall resort to collective punishment as a deterrent to ensure community pressure on the potential
-			raggers.</p>
+			institutional authorities with the local police authorities.
+		</p>
+	</li>
 
-		<h2 class="sub-heading">ANTI-RAGGING MEASURES SUBMISSION OF
+	<li>
+		<p>
+		The Anti-Ragging Committee of the institution shall take an appropriate decision, with regard to punishment or
+		otherwise, depending on the facts of each incident of ragging and nature and gravity of the incident of ragging.
+		Depending upon the nature and gravity of the offence as established the possible punishments for those found guilty
+		of ragging at the institution level shall be any one or any combination of the following:
+		</p>
+		<ul>
+			<li><p>Cancellation of admission.</p></li>
+			<li>
+		<p>
+		Suspension from attending classes.
+		</p>
+	</li>
+
+	<li>
+		<p>
+		Withholding / withdrawing scholarship / fellowship and other benefits.
+		</p>
+	</li>
+
+	<li>
+		<p>Debarring from appearing in any test / examination or other evaluation process
+		Withholding results.</p>
+	</li>
+	<li><p>Debarring from representing the institution in any regional, national or international meet, tournament, youth
+		festival, etc.</p></li>
+
+		<li><p>Suspension / expulsion from the hostel.</p></li>
+
+		<li><p>Rustication from the institution for period ranging from 1 to 4 semesters.</p></li>
+
+		<li><p>Expulsion from the institution and consequent debarring from admission to any other institution.</p></li>
+
+		<li><p><strong>Collective punishment:</strong> when the persons committing or abetting the crime of ragging are not identified, the
+		institution shall resort to collective punishment as a deterrent to ensure community pressure on the potential
+		raggers.</p></li>
+		</ul>
+	</li>
+
+</ul>
+
+			</div>
+		</div>
+	</div>
+
+<div class="row">
+	<div class="col-12">
+	<h2 class="sub-heading">ANTI-RAGGING MEASURES SUBMISSION OF
 			AFFIDAVITS BY THE STUDENTS / PARENT / GUARDIAN SEEKING ADMISSION TO THE INSTITUTE</h2>
 
 		<p>
@@ -86,55 +172,88 @@ $page_requirements = '
 			with the registration.<br><br> It is further, requested that this information be passed
 			amongst friends. Best wishes, Prof.(Dr.) Asit Kumar Saha, Principal, Haldia Institute of
 			Technology, Haldia, WBSteps for filling up the ONLINE anti-ragging affidavit It is a
-			simple procedure comprising 5 steps<br>
-			Step 1: Log on to
-			http://www.amanmovement.org<br><br>
-
-			Step 2: Click on the button called – Online Undertaking
-			(Affidavit)<br><br>
-
-			Step 3: Fill in the information as desired and submit the form.<br><br>
-
-			Step 4: On successful completion you will receive affidavits,
+			simple procedure comprising 5 steps.
+			<br>
+			<br>
+			<b>Step 1:</b> Log on to
+			http://www.amanmovement.org
+			<br>
+			<br>
+			<b>Step 2:</b> Click on the button called – Online Undertaking
+			(Affidavit)
+			<br>
+			<br>
+			<b>Step 3:</b> Fill in the information as desired and submit the form.
+			<br>
+			<br>
+			<b>Step 4:</b> On successful completion you will receive affidavits,
 			both for Students and Parents, through E mail. If you do not have an E mail
 			address please create one before you log in. If your parents do not have an E
 			Mail/Mobile/ Landline Phone number please do not panic. You can give those of
 			your friends or relatives. There is absolutely nothing to worry. If you make a
 			mistake while submitting your form you can start a fresh and submit the
-			information again. There is no problem. It is a very easy process.<br><br>
-
-			Step 5: Take print of both forms, yours own
-			and parents’, sign in proper places and submit to the
-			institute at the time of admission.<br><br>
-
-			You are required to have the following information before filling
-			up the form:<br><br>
-			Student details <br> *Student’s last name : *Student’s
-			first name : *Student’s mobile number : *Student’s friends mobile no in case of
-			emergency : *Landline number : *Student’s email ID : *Permanent Address 1 :
-			<br><br>
-			Parent/ Guardian details *Parent/Guardian Name :
-			*Parent/Guardian Address 1 : *Residence Phone No : *Mobile No of Parent/guardian
-			: *Parent/guardian’s Email ID:
-			<br><br>
-			College details
+			information again. There is no problem. It is a very easy process.
 			<br>
-			*State in which college is : West Bengal<br> *Is it a professional
-			or general college: Professional <br>*Name of the college :
-			Haldia Institute of Technology<br> *Name of Affiliating University :
-			Maulana Abul Kalam Azad University of technology (Formerly WBUT) <br>*Is it deemed university :
-			NO <br>*Director/Principal last name : Saha
-			*Director/Principal first name : Asit Kumar *Director/Principal
+			<br>
+			<b>Step 5:</b> Take print of both forms, yours own
+			and parents’, sign in proper places and submit to the
+			institute at the time of admission.
+			<br>
+			<br>
+			<strong>You are required to have the following information before filling
+			up the form:</strong>
+			<br>
+		</p>
+			<div class="boxyfy">
+			<b>Student details</b>
+			<br>
+			*Student’s first name : <br>
+			*Student’s last name :<br>
+			*Student’s mobile number :<br>
+			*Student’s friends mobile no in case of
+			emergency : <br>
+			*Landline number : <br>
+			*Student’s email ID : <br>
+			*Permanent Address 1 :
+			<br><br>
+			<b>Parent/ Guardian details </b>
+			<br>
+			*Parent/Guardian Name :<br>
+			*Parent/Guardian Address 1 :<br>
+			 *Residence Phone No : <br>
+			 *Mobile No of Parent/guardian : <br>
+			 *Parent/guardian’s Email ID:
+			<br><br>
+			<b>College details</b>
+			<br>
+			*State in which college is : West Bengal<br>
+			*Is it a professional
+			or general college: Professional <br>
+			*Name of the college :
+			Haldia Institute of Technology<br>
+			*Name of Affiliating University :
+			Maulana Abul Kalam Azad University of technology (Formerly WBUT) <br>
+			*Is it deemed university :
+			NO <br>
+			*Director/Principal last name : Saha <br>
+			*Director/Principal first name : Asit Kumar <br>
+			*Director/Principal
 			gender : Male<br><br>
 
-			*College phone no 1 : 03224 252900 *Nearest police station name
-			and address : Haldia Police Station, Chiranjibpur, Haldia
-
-		</p>
-
+			*College phone no 1 : 03224 252900 <br>
+			<b>*Nearest police station name
+			and address </b>: Haldia Police Station, Chiranjibpur, Haldia
+			</div>
 	</div>
 </div>
-<div id="arc_table" class="card">
+
+
+
+
+</div>
+
+
+<div id="arc_table" class="boxyfy">
 	<h1 class="heading">Anti-Ragging Helpline</h1>
 	<table style="width:100%">
 		<tr>
